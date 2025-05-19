7259e8382c1502c8cfb0ee768535483f23918e62
pankaj <0xpankaj@gmail.com>
2025-05-20 05:19:23 +0545

    added redis pool

---
+++ crates/redis-lib/src/pool.rs
@@ -0,0 +1,18 @@
+use anyhow::Ok;
+use bb8::{Pool, PooledConnection};
+use bb8_redis::RedisConnectionManager;
+pub struct RedisPool {
+    pool: Pool<RedisConnectionManager>,
+}
+
+impl RedisPool {
+    pub async fn new(redis_url: &str) -> anyhow::Result<Self> {
+        let manager = RedisConnectionManager::new(redis_url).unwrap();
+        let pool = Pool::builder().max_size(100).build(manager).await?;
+        Ok(RedisPool { pool })
+    }
+
+    pub async fn get_conn(&self) -> anyhow::Result<PooledConnection<RedisConnectionManager>> {
+        Ok(self.pool.get().await?)
+    }
+}
